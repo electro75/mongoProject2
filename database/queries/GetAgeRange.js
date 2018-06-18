3d5e5c5ede4ec1b47d5e0c356ce44f662b9e8ce2
sahil n <sahil@mutterfly.in>
2018-06-18 18:20:52 +0530

    refactors code to make it more performant.

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,12 +6,21 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
-    return Artist.find({})
-                 .sort({age : 1})
-                 .then(artists =>{
-                     return {
-                        min: artists[0].age,
-                        max: artists[artists.length-1].age
-                    }
-                })
+    const minQuery =  Artist.find({})
+                            .sort({age : 1})
+                            .limit(1)
+                            .then(artists => artists[0].age)
+                            
+    const maxQuery = Artist.find({})
+                           .sort({age : -1})
+                           .limit(1)
+                           .then(artists => artists[0].age)
+    
+    return Promise.all([minQuery, maxQuery])
+                  .then((result) =>  {
+                      return {
+                        min: result[0],
+                        max: result[1]
+                    }  
+                })                        
 };
--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -6,13 +6,22 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
-    return Artist.find({})
-                .sort({yearsActive: 1})
-                .then(artists =>{
-                    return {
-                        min: artists[0].yearsActive,
-                        max: artists[artists.length - 1].yearsActive
-                    }
-                })
+    const minQuery =  Artist.find({})
+                            .sort({yearsActive : 1})
+                            .limit(1)
+                            .then(artists => artists[0].yearsActive)
+                            
+    const maxQuery = Artist.find({})
+                           .sort({yearsActive : -1})
+                           .limit(1)
+                           .then(artists => artists[0].yearsActive)
+    
+    return Promise.all([minQuery, maxQuery])
+                  .then((result) =>  {
+                      return {
+                        min: result[0],
+                        max: result[1]
+                    }  
+                })  
 
 };
